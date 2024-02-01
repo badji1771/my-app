2a318dbc507b88818234a01cce2f901cb2bcd0bb
mbadji <mbadji1771@gmail.com>
2024-02-01 09:19:59 +0000

    first commit angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(){
+    this.title$.subscribe(this.setTitle)
+    //this.changeTitle(this.setTitle);
+    //this.onComplete().then(this.setTitle);
+  }
+  private setTitle = () =>{
+    const timestamp = new Date().getMilliseconds();
+    this.title = `new Title ${timestamp}`
+  }
+
+  title$ = new Observable(observable =>{
+    setInterval(()=>{
+      observable.next();
+    },2000);
+  })
+
+  private changeTitle(callback: {():void;(): void;}){
+    setInterval(()=>{
+      callback();
+    },2000);
+  }
+  private onComplete(){
+    return new Promise<void>((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve();
+      },2000);
+    });
+  }
 }
